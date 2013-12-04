4b10ce2f9c5b3c83845b6c1d7ad268c104ca9119
Eric Potash <eric@k2co3.net>
2013-12-04 15:55:10 -0600

    initial commit

---
+++ src/main/java/VisibleHandCLI.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import visiblehand.VisibleHand;
+import visiblehand.entity.Flight;
+import visiblehand.parser.air.AirParser;
+
+public class VisibleHandCLI {
+	static final Logger logger = LoggerFactory.getLogger(VisibleHandCLI.class);
+
+	public static void main(String[] args) throws MessagingException,
+			ParseException, IOException {
+		VisibleHand.initEbean();
+		Folder inbox = VisibleHand.getInbox();
+		List<Flight> flights = new ArrayList<Flight>();
+
+		for (AirParser parser : VisibleHand.airParsers) {
+			if (parser.isActive()) {
+				for (Message message : inbox.search(parser.getSearchTerm())) {
+					logger.info(message.getSubject());
+					flights.addAll(parser.parse(message).getFlights());
+				}
+			}
+		}
+
+		VisibleHand.printStatistics(flights);
+		inbox.close(false);
+	}
+}
