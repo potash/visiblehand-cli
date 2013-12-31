5f852bd79bf520b3dd608bc6cc8bb4ea90220e1d
Eric Potash <eric@k2co3.net>
2013-12-30 20:09:41 -0600

    exception handling

--- src/main/java/VisibleHandCLI.java
+++ src/main/java/VisibleHandCLI.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -11,14 +12,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import visiblehand.VisibleHand;
+import visiblehand.entity.air.AirReceipt;
 import visiblehand.entity.air.Flight;
 import visiblehand.parser.air.AirParser;
 
 public class VisibleHandCLI {
 	static final Logger logger = LoggerFactory.getLogger(VisibleHandCLI.class);
 
-	public static void main(String[] args) throws MessagingException,
-			ParseException, IOException {
+	public static void main(String[] args) throws MessagingException, FileNotFoundException, IOException {
 		VisibleHand.initEbean();
 		Folder inbox = VisibleHand.getInbox();
 		List<Flight> flights = new ArrayList<Flight>();
@@ -27,7 +28,12 @@ public class VisibleHandCLI {
 			if (parser.isActive()) {
 				for (Message message : inbox.search(parser.getSearchTerm())) {
 					logger.info(message.getSubject());
-					flights.addAll(parser.parse(message).getFlights());
+					try {
+						AirReceipt receipt = parser.parse(message);
+						flights.addAll(receipt.getFlights());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
